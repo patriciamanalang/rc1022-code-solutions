d9c1aa5b18ad9c9ebf8ebd30913a944f7a7cb50a
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-18 17:19:02 +0000

    updated javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -6,7 +6,9 @@ var orderHistory = [
     itemCategory: 'book',
     returnWindowClosed: 'September 7, 2020',
     itemName: 'JavaScript for impatient programmers',
-    shipTo: 'JS Masher'
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    priceBeforeTax: '$31.55'
 
   },
   {
@@ -16,7 +18,9 @@ var orderHistory = [
     itemCategory: 'book',
     returnWindowClosed: 'August 19, 2020',
     itemName: 'The Timeless Way of Building',
-    shipTo: 'JS Masher'
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    priceBeforeTax: '$41.33'
 
   },
   {
@@ -26,7 +30,9 @@ var orderHistory = [
     itemCategory: 'electronics',
     returnWindowClosed: 'August 5, 2020',
     itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    shipTo: 'JS Masher'
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    priceBeforeTax: '$15.98'
 
   },
   {
@@ -36,6 +42,8 @@ var orderHistory = [
     itemCategory: 'book electronics',
     returnWindowClosed: 'August 4, 2020',
     itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch), The Art of Sql',
-    shipTo: 'JS Masher'
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    priceBeforeTax: '$94.95 $33.99'
   }
 ];
