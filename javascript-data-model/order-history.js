708603df4b0e4d95e813cb8ba03c0ac305876cd3
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-18 19:37:59 +0000

    updated javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,7 +3,7 @@ var orderHistory = [
     orderPlaced: 'August 4, 2020',
     orderTotalPrice: '$34.00',
     expectedDelivery: 'August 8, 2020',
-    itemDescription: {
+    orderDescription: {
       category: 'book',
       itemName: 'JavaScript for impatient programmers',
       priceBeforeTax: '$31.55'
@@ -16,7 +16,7 @@ var orderHistory = [
     orderPlaced: 'July 19, 2020',
     orderTotalPrice: '$44.53',
     expectedDelivery: 'July 20, 2020',
-    itemDescription: {
+    orderDescription: {
       category: 'book',
       itemName: 'The Timeless Way of Building',
       priceBeforeTax: '$41.33'
@@ -29,7 +29,7 @@ var orderHistory = [
     orderPlaced: 'July 4, 2020',
     orderTotalPrice: '$17.22',
     expectedDelivery: 'July 7, 2020',
-    itemDescription: {
+    orderDescription: {
       category: 'electronics',
       itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
       priceBeforeTax: '$41.33'
@@ -43,16 +43,19 @@ var orderHistory = [
     orderPlaced: 'July 3, 2020',
     orderTotalPrice: '$138.93',
     expectedDelivery: 'July 5, 2020',
-    item1Description: {
-      category: 'electronics',
-      itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-      priceBeforeTax: '$94.95'
-    },
-    item2Description: {
-      category: 'book',
-      itemName: 'The Art of Sql',
-      priceBeforeTax: '$33.99'
-    },
+    orderDescription:
+    [
+      {
+        category: 'electronics',
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        priceBeforeTax: '$94.95'
+      },
+      {
+        category: 'book',
+        itemName: 'The Art of Sql',
+        priceBeforeTax: '$33.99'
+      }
+    ],
     orderReturnWindowClosedDate: 'August 4, 2020',
     shipTo: 'JS Masher',
     orderNumber: '114-2875557-9059409'
