6b9c2d321a8a3df98f3cbdc6d5a3bd2fc985079c
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-18 16:54:28 +0000

    completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,33 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    totalPrice: '$34.00',
+    expectedDelivery: 'August 8, 2020',
+    itemCategory: 'book',
+    returnWindowClosed: 'September 7, 2020'
+
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    totalPrice: '$44.53',
+    expectedDelivery: 'July 20, 2020',
+    itemCategory: 'book',
+    returnWindowClosed: 'August 19, 2020'
+
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    totalPrice: '$17.22',
+    expectedDelivery: 'July 7, 2020',
+    itemCategory: 'electronics',
+    returnWindowClosed: 'August 5, 2020'
+
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    totalPrice: '$138.93',
+    expectedDelivery: 'July 5, 2020',
+    itemCategory: 'book electronics',
+    returnWindowClosed: 'August 4, 2020'
+  }
+];
