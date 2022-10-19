1feac58e9da79f6bc0c24698bec650c2f588549b
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-19 00:51:51 +0000

    completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-length-of-string.js
@@ -0,0 +1,5 @@
+/* exported getLengthOfString */
+function getLengthOfString(string) {
+  var lengthOfString = string.length;
+  return lengthOfString;
+}
---
+++ javascript-functions-practice/get-sum-of-squares.js
@@ -0,0 +1,5 @@
+/* exported getSumOfSquares */
+function getSumOfSquares(x, y) {
+  var sumOfSquares = (x * x) + (y * y);
+  return sumOfSquares;
+}
