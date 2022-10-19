1abc63c1cdcbb223950130d368f6c1cebe30733c
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-19 04:37:34 +0000

    revised javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,49 +1,64 @@
 var orderHistory = [
   {
+    numberOfOrders: 1,
     orderPlaced: 'August 4, 2020',
     orderTotalPrice: '$34.00',
     expectedDelivery: 'August 8, 2020',
-    orderDescription: {
-      category: 'book',
-      itemName: 'JavaScript for impatient programmers',
-      priceBeforeTax: '$31.55'
-    },
     orderReturnWindowClosedDate: 'September 7, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '114-3941689-8772232'
+    orderNumber: '114-3941689-8772232',
+    shoppingCart:
+    [
+      {
+        category: 'book',
+        itemName: 'JavaScript for Impatient programmers',
+        priceBeforeTax: '$31.55'
+      }
+    ]
   },
   {
+    numberOfOrders: 1,
     orderPlaced: 'July 19, 2020',
     orderTotalPrice: '$44.53',
     expectedDelivery: 'July 20, 2020',
-    orderDescription: {
-      category: 'book',
-      itemName: 'The Timeless Way of Building',
-      priceBeforeTax: '$41.33'
-    },
     orderReturnWindowClosedDate: 'August 19, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '113-9984268-1280257'
+    orderNumber: '113-9984268-1280257',
+    shoppingCart:
+    [
+      {
+        category: 'book',
+        itemName: 'The Timeless Way of Building',
+        priceBeforeTax: '$41.33'
+      }
+    ]
   },
   {
+    numberOfOrders: 1,
     orderPlaced: 'July 4, 2020',
     orderTotalPrice: '$17.22',
     expectedDelivery: 'July 7, 2020',
-    orderDescription: {
-      category: 'electronics',
-      itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-      priceBeforeTax: '$41.33'
-    },
     orderReturnWindowClosedDate: 'August 5, 2020',
     shipTo: 'JS Masher',
-    orderNumber: '114-2875557-9059409'
-
+    orderNumber: '114-2875557-9059409',
+    shoppingCart:
+    [
+      {
+        category: 'electronics',
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        priceBeforeTax: '$41.33'
+      }
+    ]
   },
   {
+    numberOfOrders: 2,
     orderPlaced: 'July 3, 2020',
     orderTotalPrice: '$138.93',
     expectedDelivery: 'July 5, 2020',
-    orderDescription:
+    orderReturnWindowClosedDate: 'August 4, 2020',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    shoppingCart:
     [
       {
         category: 'electronics',
@@ -55,9 +70,6 @@ var orderHistory = [
         itemName: 'The Art of Sql',
         priceBeforeTax: '$33.99'
       }
-    ],
-    orderReturnWindowClosedDate: 'August 4, 2020',
-    shipTo: 'JS Masher',
-    orderNumber: '114-2875557-9059409'
+    ]
   }
 ];
