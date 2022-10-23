892d2ad57df19472a56bc346f85529014d0471bc
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-23 03:51:49 +0000

    completed array-manipulation-1 exercise.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,4 +1,15 @@
 /* exported compact */
-function compact(array) {
+// create a storage for output
+// create a for loop to go through entire array
+// create an if statement to find truthy values
+// add truthy values on the storage you created
 
+function compact(array) {
+  var truthyValues = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      truthyValues.push(array[i]);
+    }
+  }
+  return truthyValues;
 }
