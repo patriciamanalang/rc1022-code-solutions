6b556765113f7dfaf1e1c8f0840459555758af22
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-23 22:11:09 +0000

    completed object-manipulation-1 exercise.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,4 +1,10 @@
 /* exported toObject */
-function toObject(keyValuePair) {
 
+// create storage for output
+// use bracket notation to assign keyvaluepair at zero to equal to keyvaluepair at one
+// return variable for output
+function toObject(keyValuePair) {
+  var object = {};
+  object[keyValuePair[0]] = keyValuePair[1];
+  return object;
 }
