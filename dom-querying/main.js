1b2d86660c0496e115181d02c4155ceebf93f211
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-24 18:00:27 +0000

    completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var heading = document.querySelector('h1');
+console.log('heading');
+console.dir(heading);
+
+var explanation = document.querySelector('#explanation');
+console.log('explanation');
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('hint');
+console.dir(hint);
+
+var p = document.querySelectorAll('p');
+console.dir(p);
+
+var link = document.querySelectorAll('.example-link');
+console.log(link);
