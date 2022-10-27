fbd343e26e68f812fc576f1833d9c8c1ceb5bd56
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-27 06:32:45 +0000

    IN PROGRESS.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,10 @@
+// Query DOM for ALL span elements
+// Set up a variable for the index number
+// Start the first span element in the default stat
+
+// Declare a function
+// if the user's input matches the letter:
+// change formatting to correct
+// then move on to the next letter and change styling to default
+
+// else, change styling to wrong, they can't move on until they type the correct letter
