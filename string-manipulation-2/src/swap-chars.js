2a30c2c00ecc9a3dc3dfa04713328d2d6bac02b4
Patricia Manalang <patriciamanalang06@gmail.com>
2022-10-31 05:05:03 +0000

    completed string-manipulation-2.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,29 @@
 /* exported swapChars */
+
+// loop over the characters
+// slice each character
+// create an empty array to push each character
+// if the index value is the firstIndex, save it in a variable
+// push a space character as placeholder
+// if the index value is the secondIndex, save it in a variable
+// push a space character as placeholder
+// if the index value is not the firstIndex or the secondIndex push the character at the index
+// reassign the value of character at second index of array to the firstChar variable
+// reassign the value of character at first index of array to the secondChar variable
+// join the return
+
+function swapChars(firstIndex, secondIndex, string) {
+  var firstChar = string[firstIndex];
+  var secondChar = string[secondIndex];
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      newString += secondChar;
+    } else if (i === secondIndex) {
+      newString += firstChar;
+    } else {
+      newString += string.slice(i, i + 1);
+    }
+  }
+  return newString;
+}
