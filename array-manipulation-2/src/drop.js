f602355b94127321cef352abd56d9f27195b031e
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-08 05:15:25 +0000

    IN PROGRESS.

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,10 @@
 /* exported drop */
+// Create a function
+// Create a variable  to hold the new array
+// Slice the arrray at the designated count
+// return the new array
+
+function drop(array, count) {
+  var newArray = array.slice(count);
+  return newArray;
+}
