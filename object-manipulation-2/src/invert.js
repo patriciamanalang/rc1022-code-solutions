699480eab79868faa58e8b58774a8de9a5eba7e1
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-09 04:01:49 +0000

    IN PROGRESS.

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,13 @@
 /* exported invert */
+// create new object
+// utilize for in loop to loop over the source object's key
+// reassign the new object's key as the source object's value
+// reassign the new object's value as source object's key
+
+function invert(source) {
+  var newObj = {};
+  for (var key in source) {
+    newObj[source[key]] = key;
+  }
+  return newObj;
+}
