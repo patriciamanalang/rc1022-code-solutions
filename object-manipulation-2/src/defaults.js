55d8281f2415586c5206659a9e80c10579843982
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-11 05:53:58 +0000

    completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,13 @@
 /* exported defaults */
+// use a for in loop
+// loop over the source object to gain access to its keys
+// if the target object at keys = undefined
+// create a new key value pair for the target object using the source object's key value pair
+
+function defaults(target, source) {
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+}
