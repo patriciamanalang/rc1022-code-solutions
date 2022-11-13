bb9099db1164246719928e8925807c71e8bf5a07
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-13 16:45:05 +0000

    Completed array-manipulation-2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,15 @@
 /* exported chunk */
+// create an empty array
+// loop over the array
+// increment the i by the size
+// slice the array at the current index up to the index + size
+// push the returned array into the final array
+// return the final array
+
+function chunk(array, size) {
+  var finalArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    finalArray.push(array.slice(i, size + i));
+  }
+  return finalArray;
+}
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,15 @@
 /* exported includes */
+// Declare a function
+// Loop through the array
+// If an item in the array matches value, return true
+// otherwise return false
+// Stop loop at the end of the array
+
+function includes(array, value) {
+  var i = 0;
+  while (i < array.length) {
+    if (array[i] === value) return true;
+    i++;
+  }
+  return false;
+}
