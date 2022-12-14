b7ca2dea03912d0829ea9be7590cc3df31c6c4c6
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-14 04:59:16 +0000

    completed POST.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -9,7 +9,6 @@ const db = new pg.Pool({
 });
 
 const express = require('express');
-
 const app = express();
 
 app.listen(3000, () => {
@@ -19,6 +18,7 @@ app.listen(3000, () => {
 
 app.use(express.json());
 
+// GET
 app.get('/api/grades', (req, res, next) => {
 
   const sql = `
@@ -36,3 +36,32 @@ app.get('/api/grades', (req, res, next) => {
       res.status(500).json({ error: 'An unexpected error occurred.' });
     });
 });
+
+// POST
+app.post('/api/grades/', (req, res) => {
+  const newGrade = req.body;
+  const score = Number(newGrade.score);
+  if (!('name' in newGrade) || !('course' in newGrade) || !('score' in newGrade)) {
+    res.status(400).json({ error: 'name, course, and score are required fields' });
+  } else if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).json({ error: 'score must be an integer between 0 and 100' });
+  } else {
+    const sql = `
+    insert into "grades" ("name", "course", "score")
+    values ($1, $2, $3)
+    returning *;
+  `;
+    const param = [newGrade.name, newGrade.course, newGrade.score];
+    db.query(sql, param)
+      .then(result => {
+        const grade = result.rows[0];
+        res.status(201).json(grade);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occured.' });
+      });
+  }
+});
+
+// PUT
