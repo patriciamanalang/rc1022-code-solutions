fac2f38339046598d0e7753fce725faff9649937
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-16 23:29:04 +0000

    completed react-stopwatch-component.

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Stopwatch from './stopwatch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Stopwatch/>);
