2fb112011576d9bb353f627bf84cf0fb5a075e58
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-24 06:50:35 +0000

    set up files and dependencies.

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Accordion from './accordion';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Accordion />);
