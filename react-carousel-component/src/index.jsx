376329e10842749127b96a03f69f3af7d0d23176
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-24 07:00:03 +0000

    set up files and dependencies.

---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Carousel from './carousel';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Carousel />);
