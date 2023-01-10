afeeffdca7244557dc9658148635797faec4eb64
Patricia Manalang <patriciamanalang06@gmail.com>
2023-01-10 22:23:23 +0000

    completed data-structures-queues

---
+++ data-structures-queues/take-2nd.js
@@ -0,0 +1,10 @@
+/* exported take2nd */
+
+function take2nd(queue) {
+  if (queue.peek() === undefined) {
+    return undefined;
+  }
+  const front = queue.dequeue();
+  queue.enqueue(front);
+  return queue.dequeue();
+}
